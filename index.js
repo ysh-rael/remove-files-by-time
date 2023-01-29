13b1c5d4d4dd4a7ae909b6a30be57677ccebaa3b
Ysh-rael_linux <ysp.rael@gmail.com>
2023-01-29 03:15:13 -0300

    stable version

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function getAllFilesOfAllDir(req, res, next) {
         const readDirectories = fs.readdirSync(_path) // ler diretórios atuais
 
         readDirectories.forEach(nameOfThis => { // para cada item contido dentro desse diretório, faça...
-            stat = fs.statSync(`${_path}/${nameOfThis}`) // pegar as info dele
-            if (stat.isDirectory()) getAllFilesOfDir(`${_path}/${nameOfThis}`) // se for um diretório, a função chama ela mesmo passando o caminho atual
-            else salveOut.push(`${_path}/${nameOfThis}`) // se for arquivo envia para a variável fora da função
+            stat = fs.statSync(`${_path}/${nameOfThis}`) // pegar as info dele.
+            if (stat.isDirectory()) getAllFilesOfDir(`${_path}/${nameOfThis}`) // se for um diretório, a função chama ela mesmo passando o caminho atual.
+            else salveOut.push(`${_path}/${nameOfThis}`) // se for arquivo envia para a variável fora da função.
         })
     }
 
@@ -17,13 +17,16 @@ function getAllFilesOfAllDir(req, res, next) {
     return salveOut
 }
 
-function treatFiles({ files, keepHowMany, executeDelete, except }) {
-    // files: array com arquivos
-    // keepHowMany: quantidade mínima de arquivos que deve ser deixados
-    // executeDelete: executa a exclusão dos dados
-    // retorno da função: Todos arquivos encontrados na busca.
+function deleteFiles({ files, keepHowMany, executeDelete, except }) {
+    // files: Array com caminho dos arquivos.
+    // keepHowMany: Quantidade mínima de arquivos que deve ser deixados.
+    // executeDelete: Executa a exclusão dos dados.
+    // except: auto explicativo. arquivos que serão desconsiderados.
+    // retorno da função: Todos arquivos encontrados na busca ou 'false' em caso de erro.
 
-    if (typeof except != 'object') except = [except]
+    if (!files) return false //função fica sem sentido sem arquivos para ler, não é mesmo?
+
+    if (typeof except != 'object') except = [except] // tendencioso a erro dependendo do tipo recebido(erro será tratado com 'catch').
 
     const [salveThis, removeTheseIndex, result] = [[], [], []]
     let dateComplete = []
@@ -36,18 +39,20 @@ function treatFiles({ files, keepHowMany, executeDelete, except }) {
         })
     }
 
-    {   // "remove as excessões da contagem"
+    try {   // "remove as excessões da contagem"
         result.forEach((e, i) => except.forEach(_except => { if (_except == e.file) removeTheseIndex.push(i) && console.log("This file is except and d'not exclued: ", e.file) }))
         removeTheseIndex.forEach(e => result.splice(e, 1) && dateComplete.splice(e, 1))
-    }
+    } catch (err) { console.log('Err - except invalid: ', err); return false }
 
-    {
+    {       // keepHowMany
+        if (typeof keepHowMany !== 'number' || keepHowMany < 0) keepHowMany = -1
+        keepHowMany = parseInt(keepHowMany)
         dateComplete = dateComplete.reverse() //Ordena do mais atual ao mais antigo
-        if (keepHowMany > 0) { let i = 0; while (i < keepHowMany) salveThis.push(dateComplete[i]) && i++ } // keepHowMany
+        if (keepHowMany > 0) { let i = 0; while (i < keepHowMany) salveThis.push(dateComplete[i]) && i++ }
     }
 
-    {
-        result.forEach(result => { // executa a exclusão dos arquivos
+    {      // executa a exclusão dos arquivos
+        result.forEach(result => { 
             const keep = salveThis.includes(result.date)
             if (keep) console.log("is keep this: ", result.file)
 
@@ -59,7 +64,7 @@ function treatFiles({ files, keepHowMany, executeDelete, except }) {
     return result
 }
 
-treatFiles({
+deleteFiles({
     files: getAllFilesOfAllDir(__dirname),
     executeDelete: true,
     keepHowMany: 1,
